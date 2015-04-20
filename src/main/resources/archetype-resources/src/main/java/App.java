f40f524523be67d87f69d529d58c9145c02429aa
Bernd Prager <bernd@prager.ws>
2015-04-19 21:24:08 -0400

    SpringBoot PrimeFaces archetype
    Maven archetype for a SpringBoot JSF PrimeFaces project

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $ws.prager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
